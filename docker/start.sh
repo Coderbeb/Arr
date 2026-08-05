4684efcd945ee1995b1c94d07267a2e523e72af2
Ritik Yadav <coderbeb@gmail.com>
2026-08-05 12:35:38 +0530

    Fix log permissions in start.sh

--- docker/start.sh
+++ docker/start.sh
@@ -8,5 +8,8 @@ php artisan view:cache
 # Run database migrations
 php artisan migrate --force
 
+# Fix permissions for logs that might have been created as root during migrations
+chown -R www-data:www-data /var/www/html/storage /var/www/html/bootstrap/cache
+
 # Start Apache in the foreground
 apache2-foreground
