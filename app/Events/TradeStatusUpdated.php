c0aec40acb87766be2db5a693c8c838fe9350041
Ritik Yadav <coderbeb@gmail.com>
2026-08-07 14:57:15 +0530

    Fix WebSocket broadcast crashing the server by queueing events

--- app/Events/TradeStatusUpdated.php
+++ app/Events/TradeStatusUpdated.php
@@ -5,11 +5,11 @@ namespace App\Events;
 use App\Models\Trade;
 use Illuminate\Broadcasting\PrivateChannel;
 use Illuminate\Broadcasting\InteractsWithSockets;
-use Illuminate\Contracts\Broadcasting\ShouldBroadcastNow;
+use Illuminate\Contracts\Broadcasting\ShouldBroadcast;
 use Illuminate\Foundation\Events\Dispatchable;
 use Illuminate\Queue\SerializesModels;
 
-class TradeStatusUpdated implements ShouldBroadcastNow
+class TradeStatusUpdated implements ShouldBroadcast
 {
     use Dispatchable, InteractsWithSockets, SerializesModels;
 
